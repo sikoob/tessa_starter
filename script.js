05a9c831df10a03ad492e95d2106b86d628db5c2
sikoob <simon.koob@gmx.net>
2022-12-20 18:45:41 +0100

    working on js

--- script.js
+++ script.js
@@ -5,28 +5,26 @@ const minutesElement =document.getElementById('minutes');
 const secondsElement =document.getElementById('seconds');
 
 //target date definition
-const newYears = `2 Jan 2023 09:00` ;
-
-//define date variables for calculating seconds left from current time point to target time point
-const currentDate =new Date();
-const newYearsDate = new Date(newYears); 
-
-//calculation of seconds left until target time
-const totalSeconds =(newYearsDate - currentDate)/ 1000;
+const newDate = `2 Jan 2023, 09:00` ;
 
 function countdown () {
+    const currentDate =new Date();
+    const newDate2 = new Date(newDate);
+
+    const totalSeconds =(newDate2 - currentDate)/ 1000;
+
     if (totalSeconds >= 0) {
 
-    const days = Math.floor(totalSeconds / 3600 / 24); // Math.floor sets full number 
-    const hours = Math.floor(((totalSeconds/3600) % 24)); // Module for hours here, how much is left until 24 is full
-    const minutes = Math.floor(totalSeconds/60) % 60; // Module for minutess here, how much is left until 60 is full
-    const seconds = Math.floor(totalSeconds) % 60;                 // How much until 60 left
-    
+    const days = Math.floor(totalSeconds / 3600 / 24); /* Math.floor sets full number */
+    const hours = Math.floor(((totalSeconds/3600) % 24)); /* Module for hours here, how much is left until 24 is full */
+    const minutes = Math.floor(totalSeconds/60) % 60; /* Module for minutess here, how much is left until 60 is full*/
+    const seconds = Math.floor(totalSeconds) % 60;                 /* How much until 60 left */
 
     daysElement.innerHTML = days;
     hoursElement.innerHTML = formatTime(hours);
     minutesElement.innerHTML = formatTime(minutes);
     secondsElement.innerHTML = formatTime(seconds);
+
     } else {
     daysElement.innerHTML = 0;
     hoursElement.innerHTML = 0;
